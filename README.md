d024658a23a478428bb653ea824b9aef87244399
Irving Cazaban <cazabarray@gmail.com>
2025-05-12 16:50:21 +0200

    mise en place du support

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-• Configurer la vérification d'email
-• Configurer le mise en mémoire du compte connecter
+• Configurer la vérification d'email | Inscription
+• Configurer la connexion automatique après la création d'un compte | Inscription
+• Voir les obligations et configurer les termes | Inscription
+• Configurer le mise en mémoire du compte connecter | Connexion
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sujet = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $message = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $statut = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tickets')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->sujet;
+    }
+
+    public function setSujet(string $sujet): static
+    {
+        $this->sujet = $sujet;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getStatut(): ?\DateTime
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?\DateTime $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
